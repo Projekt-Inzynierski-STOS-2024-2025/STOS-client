62c2d544946380f484dff2130365e2c9f64c4c56
tpalinski <tpalinski@icloud.com>
2024-06-18 20:26:42 +0200

    [chore] Add singleton

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
+from orchestrator import get_instance
+
+
 def main():
     print("Starting app")
+    o = get_instance()
 
 if __name__ == "__main__":
     main()
--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -1,2 +1,8 @@
-import orchestrator
-import types
+from orchestrator.orchestrator import Orchestrator
+from orchestrator.storage.storage import LocalStorage
+
+__instance: Orchestrator = Orchestrator(LocalStorage())
+
+def get_instance() -> Orchestrator:
+    return __instance
+
