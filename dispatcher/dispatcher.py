d9e74d2e6eafb64b2a704eb5d3c572d7f40648f4
lukrzak <lukrzak@wp.pl>
2024-06-20 22:09:46 +0200

    Added healtcheck for image

--- dispatcher/dispatcher.py
+++ dispatcher/dispatcher.py
@@ -1,29 +1,51 @@
 import docker
+from docker.models.containers import Container
 
 
-def run_container(image: str, environments: dict, ports: dict):
+def run_log_gathering(container: Container) -> None:
+    logs = container.logs(stream=True)
+    try:
+        while True:
+            line = next(logs).decode('utf-8')
+            print(line)
+    except StopIteration:
+        print("Finished collecting logs")
+
+
+def run_container(image: str, environments: dict, ports: dict, healthcheck: dict) -> Container:
+    # TODO log_config with https://docker-py.readthedocs.io/en/stable/api.html#docker.types.LogConfig
     client = docker.from_env()
     return client.containers.run(image,
-                          remove=True,
-                          #healthcheck= TODO
-                          #log_config= TODO from https://docker-py.readthedocs.io/en/stable/api.html#docker.types.LogConfig
-                          detach=True,
-                          ports=ports,
-                          environment=environments
-                          )
+                                 remove=True,
+                                 healthcheck=healthcheck,
+                                 detach=True,
+                                 ports=ports,
+                                 environment=environments
+                                 )
 
 
-# Run example:
-# x = run_container("postgres:latest",
-#               {'POSTGRES_PASSWORD': 'postgres_pass'},
-#               {'5432/tcp': '5432'},
-#               )
+def run_example():
+    db_name: str = "test_db"
+    img: str = "postgres:latest"
+    envs: dict = {
+        'POSTGRES_USER': 'postgres',
+        'POSTGRES_PASSWORD': 'admin',
+        'POSTGRES_DB': db_name,
+        'PGUSER': 'postgres'
+    }
+    img_ports: dict = {
+        '5432/tcp': '5432'
+    }
+    check = {
+        'test': ['CMD-SHELL', 'pg_isready', '-d', db_name],
+        'interval': 1_000_000_000 * 3,      # in nanoseconds. Time between healthchecks.
+        'timeout': 1_000_000_000 * 10,      # in nanoseconds. Time to consider check as hung.
+        'retries': 5,                       # Number of healthchecks before considering service as not working.
+        'start_period': 1_000_000_000 * 1   # in nanoseconds. Time to start sending healthchecks.
+    }
 
-# Logs gathering - will be useful in the future
-# d = x.logs(stream=True)
-# try:
-#     while True:
-#         line = next(d).decode('utf-8')
-#         print(line)
-# except StopIteration:
-#     print("Finished collecting logs")
+    container: Container = run_container(img, envs, img_ports, check)
+    run_log_gathering(container)
+
+
+run_example()
